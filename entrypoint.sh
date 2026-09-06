42fd88aa535b9890fc71a94fac2c2a49ee147bae
ieasxu1407 <ieasxu1407@naver.com>
2026-09-06 12:29:21 +0900

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,20 +1,36 @@
 #!/bin/bash
 set -ex
 
-if [ "${RUN_FLUXBOX,,}" != "true" ]; then
-  rm -f /app/conf.d/fluxbox.conf
+RUN_FLUXBOX=${RUN_FLUXBOX:-yes}
+RUN_XTERM=${RUN_XTERM:-yes}
+
+# VNC 비밀번호 설정
+if [ -n "${VNC_PASSWORD:-}" ]; then
+    mkdir -p /root/.vnc
+
+    x11vnc -storepasswd "$VNC_PASSWORD" /root/.vnc/passwd
+
+    chmod 600 /root/.vnc/passwd
+
+    rm -f /app/conf.d/x11vnc.conf
+
+    cat > /app/conf.d/x11vnc-password.conf <<'EOF'
+[program:x11vnc]
+command=x11vnc -forever -shared -rfbauth /root/.vnc/passwd
+autorestart=true
+EOF
 fi
 
-if [ "${RUN_TERMINAL,,}" != "true" ]; then
-  rm -f /app/conf.d/terminal.conf
-fi
+case $RUN_FLUXBOX in
+    false|no|n|0)
+        rm -f /app/conf.d/fluxbox.conf
+        ;;
+esac
 
-if [ "${RUN_FIREFOX,,}" != "true" ]; then
-  rm -f /app/conf.d/firefox.conf
-fi
-
-if [ "${RUN_DOOM,,}" != "true" ]; then
-  rm -f /app/conf.d/doom.conf
-fi
+case $RUN_XTERM in
+    false|no|n|0)
+        rm -f /app/conf.d/xterm.conf
+        ;;
+esac
 
 exec supervisord -c /app/supervisord.conf
